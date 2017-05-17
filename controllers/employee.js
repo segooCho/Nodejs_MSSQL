d3e014ed179e70042e751c3f771ed9b199861f8b
segooCho <admin@adminui-MacBook-Pro.local>
2017-05-17 19:01:11 +0900

    WorkList 1.Login : 작업 완료 (POST) 2.Barcode : 작성 완료 (POST) 3.Save :  작성 완료(PUT) 4.List : 작성 완료(POST)
    * Barcode Scan 저장 & 재조회 SP 처리
    * ListView 조회 SP 처리

--- controllers/employee.js
+++ controllers/employee.js
@@ -3,8 +3,8 @@ var httpMsgs = require('../core/httpMsgs');
 var util = require('util');
 
 //// modular approach
-exports.getLogin = function (req, res, id, password) {
-    db.executeSql("EXEC SP_APP_USER_LOGIN_REV1 '"+id+"','"+password+"'", function (data, err) {
+exports.getLogin = function (req, res, userId, password) {
+    db.executeSql("EXEC SP_APP_USER_LOGIN_REV1 '"+userId+"','"+password+"'", function (data, err) {
         if (err) {
             httpMsgs.show500(req, res, err);
         } else {
@@ -59,30 +59,51 @@ exports.getExpiryDate = function (req, res, warrantyCode, goingOutDate) {
     });
 };
 
+exports.updateBarcodeSave = function (  req
+                                        ,res 
+                                        ,serialNo
+                                        ,userSpec
+                                        ,goingOutDate
+                                        ,warrantyCode
+                                        ,buyer
+                                        ,serviceCenter
+                                        ,description
+                                        ,fileName1
+                                        ,filePath1
+                                        ,fileName2
+                                        ,filePath2
+                                        ,createdBy) {
+    try {
+        db.executeSql("EXEC SP_APP_WARRANTY_INSERT_REV1 '"+serialNo+
+                                                        "','"+userSpec+
+                                                        "','"+goingOutDate+
+                                                        "','"+warrantyCode+
+                                                        "','"+buyer+
+                                                        "','"+serviceCenter+
+                                                        "','"+description+
+                                                        "','"+fileName1+
+                                                        "','"+filePath1+
+                                                        "','"+fileName2+
+                                                        "','"+filePath2+
+                                                        "','"+createdBy+
+                                                        "'", function (data, err) {
+            if (err) {
+                httpMsgs.show500(req, res, err);
+            } else {
+                if (data.length>0) {
+                    httpMsgs.sendJson(req, res, data);
+                } else {
+                    httpMsgs.sendNoDataFound(req, res);
+                }
+            }
+        });
+    } catch (ex) {
+        httpMsgs.show500(req, res, ex);
+    }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-exports.getWarrantyPOST = function (req, res, id, date) {
-    db.executeSql("Select Barcode, Id, WarrantyType, WarrantyDate from Warranty Where id='"+id+"' And convert(varchar, SDate, 112)='"+date+"'", function (data, err) {
+exports.getWorkList = function (req, res, regDate, userId) {
+    db.executeSql("EXEC SP_APP_WARRANTYSAVE_SELECT_REV1 '"+regDate+"','"+userId+"'", function (data, err) {
         if (err) {
             httpMsgs.show500(req, res, err);
         } else {
@@ -98,141 +119,3 @@ exports.getWarrantyPOST = function (req, res, id, date) {
 
 };
 
-
-// POST request. need request body
-exports.getEmployeesPUT = function (req, res, barcode, id, warrantyCode, warrantyDate) {
-    try {
-        db.executeSql("Insert into Warranty (Barcode,Id,WarrantyType,WarrantyDate) Values('"+barcode+"','"+id+"','"+warrantyCode+"','"+warrantyDate+"')", function (data, err) {
-            if (err) {
-                httpMsgs.show500(req, res, err);
-            } else {
-                httpMsgs.sendNoDataFound(req, res);
-            }
-        });
-    } catch (ex) {
-        httpMsgs.show500(req, res, ex);
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// POST request. need request body
-exports.update = function (req, res, reqbody) {
-    try {
-        if (!reqbody) throw new error("Input not valid");
-        var data = JSON.parse(reqbody);
-        if (data) {
-            
-            if (!data.Id) throw new Error("Id not provided");
-
-            var sql = "UPDATE EMPLOYEES SET";
-            
-            var isDataProvided = false;
-                                  
-            if (data.FirstName) {
-                sql += " FirstName='" + data.FirstName + "',";
-                isDataProvided = true;
-            };
-            if (data.LasttName) {
-                sql += " LastName='" + data.LastName + "',";
-                isDataProvided = true;
-            };
-            if (data.Email) {
-                sql += " Email='" + data.Email + "',";
-                isDataProvided = true;
-            };
-            if (data.DepartmentId) {
-                sql += " DepartmentId=" + data.DepartmentId + ",";
-                isDataProvided = true;
-            };
-
-            sql = sql.slice(0, -1);//remove last comma
-            sql += " WHERE ID="+data.Id
-            
-            db.executeSql(sql, function (data, err) {
-                if (err) {
-                    httpMsgs.show500(req, res, err);
-                } else {
-                    httpMsgs.send200(req, res);
-                }
-            });
-
-        } else {
-            throw new error("Input not valid");
-        }
-
-    } catch (ex) {
-        httpMsgs.show500(req, res, ex);
-    }
-};
-
-// POST request. All POST request need request body
-exports.add = function (req, res, reqbody) {
-    try {
-        if (!reqbody) throw new error("Input not valid");
-        var data = JSON.parse(reqbody);
-        if (data) {
-            var sql = "INSERT INTO EMPLOYEES (FirstName, LastName, Email, DepartmentId) VALUES ";
-            sql += util.format("('%s','%s','%s',%d)", data.FirstName, data.LastName, data.Email, data.DepartmentId);
-
-            db.executeSql(sql, function (data, err) {
-                if (err) {
-                    httpMsgs.show500(req, res, err);
-                } else {
-                    httpMsgs.send200(req, res);
-                }
-            });
-
-        } else {
-            throw new error("Input not valid");
-        }
-
-    } catch (ex) {
-        httpMsgs.show500(req, res, err);
-    }
-
-};
-
-
-
-// POST request. need request body
-exports.delete = function (req, res, reqbody) {
-    try {
-        if (!reqbody) throw new error("Input not valid");
-        var data = JSON.parse(reqbody);
-        if (data) {
-            
-            if (!data.Id) throw new Error("Id not provided");
-            
-            var sql = "DELETE FROM EMPLOYEES";           
-            
-            sql += " WHERE ID=" + data.Id
-            
-            db.executeSql(sql, function (data, err) {
-                if (err) {
-                    httpMsgs.show500(req, res, err);
-                } else {
-                    httpMsgs.send200(req, res);
-                }
-            });
-
-        } else {
-            throw new error("Input not valid");
-        }
-
-    } catch (ex) {
-        httpMsgs.show500(req, res, ex);
-    }
-
-};
--- core/server.js
+++ core/server.js
@@ -20,20 +20,18 @@ http.createServer(function (req, res) {
             } else if (req.url === "/login") {
                 req.on('data', function(params) { 
                     var data = querystring.parse(params.toString());
-                    //console.log(data.id.toString()); 
-                    //console.log(data.password.toString()); 
-                    if (!data.id) httpMsgs.show405(req, res);
+                    console.log(data); 
+                    if (!data.userId) httpMsgs.show405(req, res);
                     if (!data.password) httpMsgs.show405(req, res);
                     console.log("Log:POST-getLogin"); 
-                    emp.getLogin(req, res, data.id, data.password);
+                    emp.getLogin(req, res, data.userId, data.password);
                 });
             } else if (req.url === "/commonSpinner") {
                 req.on('data', function(params) { 
                     var data = querystring.parse(params.toString());
-                    console.log(data.mode.toString()); 
-                    //console.log(data.param1.toString());  
+                    console.log(data); 
                     if (!data.mode) httpMsgs.show405(req, res);
-                    //param1 이 없는 경우가 있어 오류 발생
+                    //param1 이 없는 경우도 있음 오류
                     //if (!data.param1) httpMsgs.show405(req, res); 
                     console.log("Log:POST-getCommomSpinner"); 
                     emp.getCommomSpinner(req, res, data.mode, data.param1);
@@ -41,8 +39,7 @@ http.createServer(function (req, res) {
             } else if (req.url === "/barcode") {
                 req.on('data', function(params) { 
                     var data = querystring.parse(params.toString());
-                    //console.log(data.barcode.toString()); 
-                    //console.log(data.id.toString()); 
+                    console.log(data); 
                     if (!data.barcode) httpMsgs.show405(req, res);
                     if (!data.id) httpMsgs.show405(req, res);
                     console.log("Log:POST-getBarcode"); 
@@ -51,30 +48,20 @@ http.createServer(function (req, res) {
             } else if (req.url === "/expiryDate") {
                 req.on('data', function(params) { 
                     var data = querystring.parse(params.toString());
-                    //console.log(data.barcode.toString()); 
-                    //console.log(data.id.toString()); 
+                    console.log(data); 
                     if (!data.warrantyCode) httpMsgs.show405(req, res);
                     if (!data.goingOutDate) httpMsgs.show405(req, res);
                     console.log("Log:POST-getExpiryDate"); 
                     emp.getExpiryDate(req, res, data.warrantyCode, data.goingOutDate);
                 });
-
-
-
-
-
-
-
-
-            } else if (req.url === "/warranty") {
+            } else if (req.url === "/workList") {
                 req.on('data', function(params) { 
                     var data = querystring.parse(params.toString());
-                    //console.log(data.id.toString()); 
-                    //console.log(data.date.toString()); 
-                    if (!data.id) httpMsgs.show405(req, res);
-                    if (!data.date) httpMsgs.show405(req, res);
-                    console.log("Log:POST-getWarrantyPOST"); 
-                    emp.getWarrantyPOST(req, res, data.id, data.date);
+                    console.log(data); 
+                    if (!data.regDate) httpMsgs.show405(req, res);
+                    if (!data.userId) httpMsgs.show405(req, res);
+                    console.log("Log:POST-getWorkList"); 
+                    emp.getWorkList(req, res, data.regDate, data.userId);
                 });
             } else {
                 httpMsgs.show404(req, res);
@@ -82,61 +69,37 @@ http.createServer(function (req, res) {
             break;
         case "PUT":
             ////POST request should have request body
-            if (req.url === "/product") {
+            if (req.url === "/barcodeSave") {
                 req.on('data', function(params) { 
                     var data = querystring.parse(params.toString());
-                    //console.log(data.id.toString()); 
-                    //console.log(data.password.toString()); 
-                    if (!data.barcode) httpMsgs.show405(req, res);
-                    if (!data.id) httpMsgs.show405(req, res);
+                    console.log(data); 
+                    if (!data.serialNo) httpMsgs.show405(req, res);
+                    if (!data.userSpec) httpMsgs.show405(req, res);
+                    if (!data.goingOutDate) httpMsgs.show405(req, res);
                     if (!data.warrantyCode) httpMsgs.show405(req, res);
-                    if (!data.warrantyDate) httpMsgs.show405(req, res);
-                    //res.writeHead(200, {'Content-Type':'text/html'});
-                    console.log("Log:PUT-getEmployeesPUT"); 
-                    emp.getEmployeesPUT(req, res, data.barcode, data.id, data.warrantyCode, data.warrantyDate);
+                    if (!data.buyer) httpMsgs.show405(req, res);
+                    if (!data.serviceCenter) httpMsgs.show405(req, res);
+                    if (!data.createdBy) httpMsgs.show405(req, res);
+                    console.log("Log:PUT-updateBarcodeSave"); 
+                    emp.updateBarcodeSave(  req 
+                                            ,res
+                                            ,data.serialNo
+                                            ,data.userSpec
+                                            ,data.goingOutDate
+                                            ,data.warrantyCode
+                                            ,data.buyer
+                                            ,data.serviceCenter
+                                            ,data.description
+                                            ,data.fileName1
+                                            ,data.filePath1
+                                            ,data.fileName2
+                                            ,data.filePath2
+                                            ,data.createdBy);
                 });
-
-                /*
-                var reqBody = '';
-                req.on("data", function (data) {
-                    reqBody += data;
-                    if (reqBody.length > 1e7) { //10 MB
-                        httpMsgs.show413(req, res);
-                    }
-                });
-                req.on("end", function () {
-                    emp.update(req, res, reqBody);
-                });
-                */
-
             } else {
                 httpMsgs.show404(req, res);
             }
             break;
-        case "DELETE":
-            ////POST request should have request body
-            if (req.url === "/employees") {
-                
-                var reqBody = '';
-                req.on("data", function (data) {
-                    reqBody += data;
-                    if (reqBody.length > 1e7) { //10 MB
-                        httpMsgs.show413(req, res);
-                    }
-                });
-                req.on("end", function () {
-                    emp.delete(req, res, reqBody);
-                });
-                
-                           
-            } else {
-                httpMsgs.show404(req, res);
-            }
-            break;
-        default:
-            httpMsgs.show404(req, res);
-            break;
-
     }
 
 
